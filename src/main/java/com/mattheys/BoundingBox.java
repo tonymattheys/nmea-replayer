8e072b4c214e5725cabe3559003f6645f8563fdd
Tony Mattheys <tony@mattheys.com>
2023-12-19 13:51:53 -0700

    Gradle-izing the project. Not quite done yet

---
+++ src/main/java/com/mattheys/BoundingBox.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mattheys;
+
+/**
+ *
+ * @author tony
+ */
+public class BoundingBox {
+    double north;
+    double south;
+    double east;
+    double west;
+}
